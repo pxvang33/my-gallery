aec3c0e202c6191232ca50980cd98e09f3ded0b6
Peng Xue Vang <pxvang33@gmail.com>
2019-01-18 16:44:28 -0600

    app.js able to append photo from array

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,6 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/manchester_city.jpg', description: 'Welcome to Manchester, entrance to Ethiad Stadium.', likes: 0}
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      gallery: [
+        {id: 1, path: <img src="images/goat_small.jpg"/>, description: 'Photo of a goat taken at Glacier National Park.', likes: 0},
+      
+      ]
+
+    } // end state
+
+  } // end constructor
   render() {
+    // for(let i = 0; i < this.state.gallery.length; i++ ){
+
+    // }
     return (
       <div className="App">
         <header className="App-header">
@@ -10,10 +25,11 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <p>{this.state.gallery[0].path}</p>
+        {/* <img src="images/goat_small.jpg"/> */}
       </div>
-    );
-  }
-}
+    ); // end return
+  } // end render
+} // end class
 
 export default App;
