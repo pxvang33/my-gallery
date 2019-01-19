a2d25fec0d039f64ee3e9527437c393c63dc2c65
Peng Xue Vang <pxvang33@gmail.com>
2019-01-19 12:38:28 -0600

    able to append more than one image to DOMusing for loop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,17 +7,22 @@ class App extends Component {
 
     this.state = {
       gallery: [
-        {id: 1, path: <img src="images/goat_small.jpg"/>, description: 'Photo of a goat taken at Glacier National Park.', likes: 0},
+        {id: 1, path: <img src="images/goat_small.jpg" alt=" "/>, description: 'Photo of a goat taken at Glacier National Park.', likes: 0},
+        { id: 2, path: <img src="images/manchester_city.jpg" alt=" "/>, description: 'Welcome to Manchester, entrance to Ethiad Stadium.', likes: 0 }
       
-      ]
-
+      ] // end this.state.gallery
     } // end state
 
   } // end constructor
+  appendPics = () => {
+    let newGallery = [];
+    for (let i = 0; i < this.state.gallery.length; i++) {
+      let galleryPic = <li>{this.state.gallery[i].path}</li>
+      newGallery.push(galleryPic)
+    }
+    return newGallery;
+  }
   render() {
-    // for(let i = 0; i < this.state.gallery.length; i++ ){
-
-    // }
     return (
       <div className="App">
         <header className="App-header">
@@ -25,7 +30,10 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <p>{this.state.gallery[0].path}</p>
+        {/* <p>{this.state.gallery[i].path}</p> */}
+        <ul>
+          {this.appendPics()}
+        </ul>
         {/* <img src="images/goat_small.jpg"/> */}
       </div>
     ); // end return
